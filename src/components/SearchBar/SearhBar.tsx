572610f2b7568e39bf3e9a9785d04d6910cff869
Musmen <musmen@internet.ru>
2023-03-18 14:39:15 +0300

    feat: add SearchBar component

---
+++ src/components/SearchBar/SearhBar.tsx
@@ -0,0 +1,48 @@
+import './SearchBar.css';
+
+import React, { ChangeEvent, FormEvent } from 'react';
+
+const DEFAULT_SEARCH_VALUE = '';
+
+class SearchBar extends React.Component<null, { searchValue: string }> {
+  state = { searchValue: DEFAULT_SEARCH_VALUE };
+
+  onClearBtnClickHandler = () => {
+    this.setState({ searchValue: DEFAULT_SEARCH_VALUE });
+  };
+
+  onSearchValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchValue: event.target.value });
+  };
+
+  onSubmitHandler = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  render() {
+    const { searchValue } = this.state;
+
+    return (
+      <form className="SearchBar" onSubmit={this.onSubmitHandler}>
+        <label className="SearchBar__label">
+          <input
+            className="SearchBar__input"
+            name="SearchBar"
+            placeholder="Input your search request, please"
+            title="Input your search request, please"
+            value={searchValue}
+            onChange={this.onSearchValueChangeHandler}
+          />
+          <button
+            className="SearchBar__clear-btn"
+            type="reset"
+            title="Clear search input"
+            onClick={this.onClearBtnClickHandler}
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
