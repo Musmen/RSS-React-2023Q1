5fdf7f6d4f04699058d453475c7d73f73edb3e3d
Musmen <musmen@internet.ru>
2023-04-01 08:18:03 +0300

    refactor: use hooks for SearchBar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,62 +1,55 @@
 import './SearchBar.css';
 
-import React, { Component, ChangeEvent, FormEvent } from 'react';
+import React, { ChangeEvent, FormEvent, useState, useEffect } from 'react';
 
 import localStorageService from '../../services/local-storage.service';
 import { LOCAL_STORAGE_KEYS } from '../../common/constants';
 
 const DEFAULT_SEARCH_VALUE = '';
 
-class SearchBar extends Component<Record<string, never>, { searchValue: string }> {
-  state = {
-    searchValue:
-      localStorageService.getFromLS(LOCAL_STORAGE_KEYS.SEARCH_VALUE) || DEFAULT_SEARCH_VALUE,
-  };
+function SearchBar() {
+  const searchValueFromLS = localStorageService.getFromLS(LOCAL_STORAGE_KEYS.SEARCH_VALUE);
+  const [searchValue, setSearchValue] = useState(searchValueFromLS || DEFAULT_SEARCH_VALUE);
 
-  onClearBtnClickHandler = () => {
-    this.setState({ searchValue: DEFAULT_SEARCH_VALUE });
+  useEffect(() => {
+    localStorageService.setToLS(searchValue, LOCAL_STORAGE_KEYS.SEARCH_VALUE);
+  }, [searchValue]);
+
+  const onClearBtnClickHandler = () => {
+    setSearchValue(DEFAULT_SEARCH_VALUE);
     localStorageService.deleteFromLS(LOCAL_STORAGE_KEYS.SEARCH_VALUE);
   };
 
-  onSearchValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+  const onSearchValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const newSearchValue: string = event.target.value;
-    this.setState({ searchValue: newSearchValue });
+    setSearchValue(newSearchValue);
     localStorageService.setToLS(newSearchValue, LOCAL_STORAGE_KEYS.SEARCH_VALUE);
   };
 
-  onSubmitHandler = (event: FormEvent) => {
+  const onSubmitHandler = (event: FormEvent) => {
     event.preventDefault();
   };
 
-  componentWillUnmount() {
-    const { searchValue } = this.state;
-    localStorageService.setToLS(searchValue, LOCAL_STORAGE_KEYS.SEARCH_VALUE);
-  }
-
-  render() {
-    const { searchValue } = this.state;
-
-    return (
-      <form className="SearchBar" onSubmit={this.onSubmitHandler}>
-        <label className="SearchBar__label">
-          <input
-            className="SearchBar__input"
-            name="SearchBar"
-            placeholder="Your search request"
-            title="Input your search request, please"
-            value={searchValue}
-            onChange={this.onSearchValueChangeHandler}
-          />
-          <button
-            className="SearchBar__clear-btn"
-            type="reset"
-            title="Clear search input"
-            onClick={this.onClearBtnClickHandler}
-          />
-        </label>
-      </form>
-    );
-  }
+  return (
+    <form className="SearchBar" onSubmit={onSubmitHandler}>
+      <label className="SearchBar__label">
+        <input
+          className="SearchBar__input"
+          name="SearchBar"
+          placeholder="Your search request"
+          title="Input your search request, please"
+          value={searchValue}
+          onChange={onSearchValueChangeHandler}
+        />
+        <button
+          className="SearchBar__clear-btn"
+          type="reset"
+          title="Clear search input"
+          onClick={onClearBtnClickHandler}
+        />
+      </label>
+    </form>
+  );
 }
 
 export default SearchBar;
