c85ad83bdf8793855de24dd37776b5ee6953e00a
Musmen <musmen@internet.ru>
2023-04-09 14:51:14 +0300

    refactor: exclude cards components common parts

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,25 +8,30 @@ interface CardProps {
   card: CardType | BigCardType;
   additionalClass?: string;
   children?: React.ReactElement;
+  onCardClickHandler?: (id: string) => void;
 }
 
-function Card({ card, additionalClass, children }: CardProps) {
-  const { title, imgSrc, author, date } = card;
+function Card({ card, additionalClass, children, onCardClickHandler }: CardProps) {
+  const { title, imgSrc, author, date, id } = card;
 
   return (
-    <section className={`Card ${additionalClass || ''}`}>
+    <section
+      className={`Card card ${additionalClass || ''}`}
+      onClick={() => onCardClickHandler && onCardClickHandler(id || '')}
+    >
       <p className="Card__wrapper Card__wrapper_title">
-        <span className="Card__label">Title:</span>
-        <span className="Card__title">{title}</span>
+        <span className="Card__label card__label">Title:</span>
+        <span className="Card__title card__title card__field">{title}</span>
       </p>
-      <img className="Card__image" src={imgSrc} alt={`${title} image`} />
-      <p className="Card__description">
+      <img className="Card__image card__image" src={imgSrc} alt={`${title} image`} />
+      <p className="Card__description card__description">
         <span className="Card__wrapper Card__wrapper_author">
-          <span className="Card__label">Author:</span>
-          <span className="Card__author">{author}</span>
+          <span className="Card__label card__label">Author:</span>
+          <span className="Card__author card__author card__field">{author}</span>
         </span>
         <span className="Card__wrapper">
-          <span className="Card__label">Date:</span> {date}
+          <span className="Card__label card__label">Date:</span>
+          <span className="Card__date card__date card__field">{date}</span>
         </span>
         {children}
       </p>
